bf22ed5a77af39f0af25bffa0cb910ec4377bca0
Adnan Ahmed <adnanahmedofficia123@gmail.com>
2026-06-28 17:03:29 +0530

    yes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div>
             <img src={assets.logo} className='mb-5 w-32' alt="" />
             <p className='w-full md:w-2/3 text-green-100'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            Buffy Tore is a fashion and lifestyle store offering trendy clothing and carefully curated accessories. The brand focuses on stylish, affordable pieces that blend everyday comfort with modern aesthetics.
             </p>
         </div>
 
