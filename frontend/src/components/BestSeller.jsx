ba9e37415221c73387208a335eaee089e3be2005
Adnan Ahmed <adnanahmedofficia123@gmail.com>
2026-06-28 18:22:02 +0530

    ji

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -20,7 +20,7 @@ const BestSeller = () => {
       <div className='text-center text-3xl py-8'>
         <Title text1={'BEST'} text2={'SELLERS'} />
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-green-100'>
-          Lorem ipsum is simple dummy text of the printing and typesetting industry. Lorem ipsum has been the standard dummy text.
+          Check out our best-selling products that our customers love! From top-rated items to customer favorites, these products have been tried and tested by our community. Don't miss out on the chance to experience the quality and satisfaction that comes with our best sellers.
         </p>
       </div>
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -17,7 +17,7 @@ const LatestCollection = () => {
       <div className='text-center py-8 text-3xl'>
         <Title text1={'LATEST'} text2={'COLLECTION'} />
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-white'>
-        Lorem ipsum is simple dummy text of the printing ad typesetting industry.Lorem ipsum hase been the.
+        Latest collection of our products, you can find the latest products in our store. We have a wide range of products for you to choose from.
         </p>
       </div>
 
