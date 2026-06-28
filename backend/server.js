def0442cd2b080ae68f09a7082e10cb2f2971719
Adnan Ahmed <adnanahmedofficia123@gmail.com>
2026-06-28 16:34:59 +0530

    update link

--- backend/server.js
+++ backend/server.js
@@ -9,25 +9,24 @@ import cartRouter from './routes/cartRoute.js'
 import orderRouter from './routes/orderRoute.js'
 import comboRouter from './routes/comboRoute.js'
 
-// App Config
 const app = express()
-const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
 
-// Middlewares
+app.use(cors({
+  origin: ['https://buffytore-admin.vercel.app', 'https://buffytore.vercel.app'],
+  credentials: true
+}))
 app.use(express.json())
-app.use(cors())
 
-// API endpoints
-app.use('/api/user',userRouter)
-app.use('/api/product',productRouter)
-app.use('/api/cart',cartRouter)
-app.use('/api/order',orderRouter)
-app.use('/api/combos',comboRouter)
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/order', orderRouter)
+app.use('/api/combos', comboRouter)
 
-app.get('/',(req,res)=>{
-    res.send("API Working")
+app.get('/', (req, res) => {
+  res.send("API Working")
 })
 
-app.listen(port, ()=> console.log('Server Started on PORT : '+ port))
+export default app
