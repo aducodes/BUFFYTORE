97bbbe4ff8359dc2ac44e974c54d6efae7bd4782
Adnan Ahmed <adnanahmedofficia123@gmail.com>
2026-06-28 16:49:03 +0530

    app

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Add from './pages/Add'
 import List from './pages/List'
 import Orders from './pages/Orders'
@@ -9,7 +9,7 @@ import AddCombo from './pages/AddCombo'
 import Login from './components/Login'
 import { ToastContainer } from 'react-toastify';
 
-export const backendUrl = import.meta.env.VITE_BACKEND_URL
+export const backendUrl = import.meta.env.VITE_BACKEND_URL.replace(/\/$/, '')
 export const currency = '₹ '
 
 
@@ -35,6 +35,7 @@ const App = () => {
             <Sidebar />
             <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
               <Routes>
+                <Route path='/' element={<Navigate to='/add' replace />} />
                 <Route path='/add' element={<Add token={token} />} />
                 <Route path='/list' element={<List token={token} />} />
                 <Route path='/orders' element={<Orders token={token} />} />
@@ -44,9 +45,6 @@ const App = () => {
           </div>
         </>
       }
-
-
-
     </div>
   )
 }
