97674d2e72f2a22dc9be95fae2dd738d574aeebc
Adnan Ahmed <adnanahmedofficia123@gmail.com>
2026-06-28 19:02:20 +0530

    CHANGE

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -9,7 +9,7 @@ const LatestCollection = () => {
     const [latestProducts,setLatestProducts] = useState([]);
 
     useEffect(()=>{
-        setLatestProducts(products.slice(0,10));
+      setLatestProducts([...products].sort((a, b) => b.date - a.date).slice(0, 10));
     },[products])
 
   return (
